651adf96b3f8f9f0a2cbf2d0d242a9b48e71fbf1
Hunter Chasens <hchcasens@gmail.com>
2019-09-18 18:21:37 -0400

    Init Algorithms interface

---
+++ src/Algorithm.java
@@ -0,0 +1,12 @@
+interface Algorithm {
+
+/**
+ * [generate description]
+ * @param  grid [is a two dimentional array of squares]
+ * @return      [a two dimentional array of squares with a generated path going through it]
+ * you can get the length of the array by using grid[0].length for the width ro grid[1].length for the y
+ */
+public Square[][] generate(Square[][] grid);
+
+
+}
